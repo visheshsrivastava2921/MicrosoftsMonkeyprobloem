dde85b04808963896db1215b11e0f938cc358e1a
visheshsrivastava2921 <124771355+visheshsrivastava2921@users.noreply.github.com>
2024-02-03 11:16:32 +0530

    Monkeycode.py

--- Monkeycode.py
+++ Monkeycode.py
@@ -1,6 +1,42 @@
 class calc:
-  def fill_table():
+  def fill_table(table,table1,comp,closed_rooms):
     z=0
+        # filling the table withe the table numbers
+        for i in range(1,11):
+            for j in range(1,11):
+                f=i*j
+                table.append(f)
+        #print(table,"\n",table1,"\n",comp,"\n",crooms)
+        #sorting the table 1
+        n=len(table)
+        for i in range(n):
+            for j in range(0,n-i-1):
+                if table[j]>table[j+1]:
+                    table[j],table[j+1]=table[j+1],table[j]
+        #print(table,"\n",table1,"\n",comp,"\n",crooms)
+        #removingt the repeating elements from the table and adding it to the now table 
+        for i in table:
+            if i not in table1:
+                table1.append(i)
+        #print(table,"\n",table1,"\n",comp,"\n",crooms)
+        # creating a new table to compare the existing table 
+        for i in range(1,101):
+            comp.append(i)
+        #print(table,"\n",table1,"\n",comp,"\n",crooms)
+        #inserting 0 in the missing elements
+        for i in range(len(comp)):
+            if table1[i]!=i:
+                table1.insert(i,0)
+        #print(table,"\n",table1,"\n",comp,"\n",crooms)
+        #calculating the number of 0 to get the number of rooms which are closed 
+        for i in range(1,99):
+            if table1[i]==0:
+                z=z+1
+                closed_rooms.append(i)
+        print(table,"\n",table1,"\n",comp,"\n",closed_rooms)
 c=calc
 table=[]
 table1=[]
+comp=[]
+closed_rooms=[]
+c.fill_table(table,table1,comp,closed_rooms)
